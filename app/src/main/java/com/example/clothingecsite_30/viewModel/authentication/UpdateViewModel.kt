248fee16f59a56b913cda6abf86eac6730dad0bf
haradatakara <takara0527nagoya@outlook.jp>
2023-02-25 15:51:32 +0900

    プロフィール更新画面 不要なコード削除 ref: #12

--- app/src/main/java/com/example/clothingecsite_30/viewModel/authentication/UpdateViewModel.kt
+++ app/src/main/java/com/example/clothingecsite_30/viewModel/authentication/UpdateViewModel.kt
@@ -61,7 +61,6 @@ class UpdateViewModel(private val updateRepository: UpdateRepository) : ViewMode
 
             viewModelScope.launch {
                 val result = updateRepository.update(updateInfo, confirmPassword.value.toString(), confirmEmail.value.toString())
-                result
                 when (val result = updateRepository.update(updateInfo, confirmPassword.value.toString(), confirmEmail.value.toString())) {
                     is com.example.clothingecsite_30.model.authentication.Result.Success -> {
                         _updateResult.value =
